17bd578e2b22fe0fef79102a1188fe004616d2ba
TaruK981221 <tarui2412git@gmail.com>
2021-02-13 14:10:43 +0900

    あああ

--- Assets/tarui's_Create/script/player/playerCon.cs
+++ Assets/tarui's_Create/script/player/playerCon.cs
@@ -12,6 +12,14 @@ public class playerCon : MonoBehaviour
     [SerializeField]
     float JumpPower = 5.0f;
 
+    bool isBound = false;
+    float BoundTimeLimit = 0.5f;
+    float BoundTime = 0.0f;
+
+    //bool is
+
+    public int HP = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,50 +29,89 @@ public class playerCon : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (isFlg)
+        if (isBound)
         {
-            if (Input.GetKey(KeyCode.LeftArrow))
+            if (BoundTime < BoundTimeLimit)
             {
-                rb.velocity = new Vector3(Vector3.left.x * 1.0f, rb.velocity.y, 0);
-            }
-            else if (Input.GetKey(KeyCode.RightArrow))
-            {
-                rb.velocity = new Vector3(Vector3.right.x * 1.0f, rb.velocity.y, 0);
+                BoundTime += Time.deltaTime;
             }
             else
             {
-                rb.velocity = new Vector3(0, rb.velocity.y, 0);
-            }
-
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                rb.velocity += Vector3.up * JumpPower;
+                BoundTime = 0.0f;
+                isBound = false;
             }
         }
         else
         {
-            if (Input.GetKey(KeyCode.A))
+            if (isFlg)
             {
-                rb.velocity = new Vector3(Vector3.left.x * 1.0f, rb.velocity.y, 0);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                rb.velocity = new Vector3(Vector3.right.x * 1.0f, rb.velocity.y, 0);
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    rb.velocity = new Vector3(Vector3.left.x * 1.0f, rb.velocity.y, 0);
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    rb.velocity = new Vector3(Vector3.right.x * 1.0f, rb.velocity.y, 0);
+                }
+                else
+                {
+                    rb.velocity = new Vector3(0, rb.velocity.y, 0);
+                }
+
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    rb.velocity += Vector3.up * JumpPower;
+                }
             }
             else
             {
-                rb.velocity = new Vector3(0, rb.velocity.y, 0);
-            }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    rb.velocity = new Vector3(Vector3.left.x * 1.0f, rb.velocity.y, 0);
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    rb.velocity = new Vector3(Vector3.right.x * 1.0f, rb.velocity.y, 0);
+                }
+                else
+                {
+                    rb.velocity = new Vector3(0, rb.velocity.y, 0);
+                }
 
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                rb.velocity += Vector3.up * JumpPower;
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    rb.velocity += Vector3.up * JumpPower;
+                }
             }
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if(collision.gameObject.tag== "Player")
+    //    {
+    //        rb.velocity = Vector3.zero;
+
+    //        if (collision.transform.position.x < transform.position.x)
+    //        {
+    //            rb.velocity += new Vector3(1, 1.5f, 0) * 1.0f;
+    //        }
+    //        else
+    //        {
+    //            rb.velocity += new Vector3(-1, 1.5f, 0) * 1.0f;
+    //        }
+
+    //        isBound = true;
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Block")
+        {
+            HP -= 1;
+
+
+        }
     }
 }
