a82ba8e93cb2d4e670c4f85c23381927b672deee
matsuzaka <mtzkyr1023@gmail.com>
2021-02-13 12:19:18 +0900

    a

---
+++ Assets/Scripts/matsuzaka/GameScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameScene : MonoBehaviour
+{
+	public GameObject rockPrefab;
+	public GameObject loadObject;
+	public float loadWidth;
+	public int raneCount;
+	public float loadScroolSpeed;
+	public float rockScroolSpeed;
+
+	private List<GameObject> rocks;
+    // Start is called before the first frame update
+    void Start()
+    {
+		Material material = loadObject.GetComponent<MeshRenderer>().material;
+		material.SetFloat("_ScrollSpeed", loadScroolSpeed);
+
+		rocks = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			GameObject rock = Instantiate(
+				rockPrefab,
+				new Vector3(Mathf.Round(Random.Range(-0.5f, 0.5f) * loadWidth * (float)raneCount), 0.5f, 8.0f),
+				Quaternion.identity);
+			rock.GetComponent<RockScript>().scrollSpeed = rockScroolSpeed;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			SceneManager.LoadScene("ResultScene");
+		}
+    }
+
+	
+}
---
+++ Assets/Scripts/matsuzaka/RockScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockScript : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float zOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(new Vector3(0.0f, 0.0f, -scrollSpeed));
+
+		if (transform.position.z < zOver)
+			Destroy(this.gameObject);
+	}
+}
