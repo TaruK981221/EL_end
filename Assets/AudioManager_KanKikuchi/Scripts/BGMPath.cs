ac661e702f6da557cd3a4899a5112e69fba5319a
matsuzaka <mtzkyr1023@gmail.com>
2021-02-13 16:18:09 +0900

    a

--- Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -5,9 +5,11 @@
 /// </summary>
 public static class BGMPath{
 
-	public const string BATTLE27    = "BGM/Battle/battle27";
-	public const string FANTASY14   = "BGM/fantasy14";
-	public const string HEARTBEAT01 = "BGM/heartbeat01";
+	public const string BATTLE27     = "BGM/Battle/battle27";
+	public const string BGM_CLEAR    = "BGM/BGM_CLEAR";
+	public const string BGM_GAMEMAIN = "BGM/BGM_GAMEMAIN";
+	public const string FANTASY14    = "BGM/fantasy14";
+	public const string HEARTBEAT01  = "BGM/heartbeat01";
 
 }
 
