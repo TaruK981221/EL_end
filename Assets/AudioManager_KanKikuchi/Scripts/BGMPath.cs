5e0c7a55789bd277b556320250b39e01d45da81b
matsuzaka <mtzkyr1023@gmail.com>
2021-02-13 16:13:47 +0900

    a

--- Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -5,11 +5,9 @@
 /// </summary>
 public static class BGMPath{
 
-	public const string BATTLE27     = "BGM/Battle/battle27";
-	public const string BGM_CLEAR    = "BGM/BGM_CLEAR";
-	public const string BGM_GAMEMAIN = "BGM/BGM_GAMEMAIN";
-	public const string FANTASY14    = "BGM/fantasy14";
-	public const string HEARTBEAT01  = "BGM/heartbeat01";
+	public const string BATTLE27    = "BGM/Battle/battle27";
+	public const string FANTASY14   = "BGM/fantasy14";
+	public const string HEARTBEAT01 = "BGM/heartbeat01";
 
 }
 
--- Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -5,12 +5,9 @@
 /// </summary>
 public static class SEPath{
 
-	public const string JINGLE10       = "SE/Jingle/jingle10";
-	public const string SE_JUMP1       = "SE/se_jump1";
-	public const string SE_PDAMAGE     = "SE/SE_PDamage";
-	public const string SE_STAGE_CLEAR = "SE/SE_StageClear";
-	public const string SYSTEM20       = "SE/system20";
-	public const string SYSTEM23       = "SE/system23";
+	public const string JINGLE10 = "SE/Jingle/jingle10";
+	public const string SYSTEM20 = "SE/system20";
+	public const string SYSTEM23 = "SE/system23";
 
 }
 
